6e2dbf9bac19a37bda3e3d56c1ce88d31a9a6f5c
Radoslaw Kopec <radoslawkopec93@gmail.com>
2023-12-17 10:03:09 +0100

    router

--- frontend-angular/src/app/app.module.ts
+++ frontend-angular/src/app/app.module.ts
@@ -1,22 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { SuccessAlertComponent } from './core/bootstrap/success-alert/success-alert.component';
 import { WarningAlertComponent } from './core/bootstrap/warning-alert/warning-alert.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+//import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', component: SuccessAlertComponent },
+// { path: '', redirectTo: '/home', pathMatch: 'full' },
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     SuccessAlertComponent,
-    WarningAlertComponent
+    WarningAlertComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule,
-    NgbModule
+    BrowserModule, RouterModule.forRoot(routes),
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+ }
